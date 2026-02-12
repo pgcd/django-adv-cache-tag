838d48ef056d4bf772a03c17c3f055465e34c35c
Paolo Dente <pgcd93@gmail.com>
2026-02-12 08:34:48 +0100

    Removes versioning altogether

--- adv_cache_tag/__init__.py
+++ adv_cache_tag/__init__.py
@@ -1,17 +1,3 @@
 """An advanced template tag for caching in django: versioning, compress, partial caching, easy inheritance"""
 
-from os import path
-from setuptools.config import read_configuration
-
-
-def _extract_version(package_name):
-    _conf = read_configuration(path.join(
-        path.dirname(__file__), '..', 'setup.cfg')
-    )
-    version = _conf['metadata']['version']
-
-    return version
-
-
-EXACT_VERSION = _extract_version('django_adv_cache_tag')
-VERSION = tuple(int(part) for part in EXACT_VERSION.split('.') if str(part).isnumeric())
+VERSION = "spacebase.dev"
